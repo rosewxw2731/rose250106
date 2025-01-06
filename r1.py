6060d5480254ef4d8cb10f1577b0460648d222f3
rosewxw2731 <rosewxw2731@gmail.com>
2025-01-06 05:08:43 +0000

    1.0

---
+++ r1.py
@@ -0,0 +1,28 @@
+'''
+for i in range(1,4):
+    print(i)
+    fa=float(input("fa="))
+    ce=5*(fa-32)/9
+    print("fa=%f,ce=%f"%(fa,ce))
+'''
+i=1
+'''
+while i<=3:
+    print(i)
+    fa=float(input("fa="))
+    ce=5*(fa-32)/9
+    print("fa=%f,ce=%f"%(fa,ce))
+    i=i+1
+while i==4:
+    print("End")
+    i=i+1
+'''
+while i<=3:
+    print(i)
+    fa=float(input("fa="))
+    ce=5*(fa-32)/9
+    print("fa=%f,ce=%f"%(fa,ce))
+    i=i+1
+while i==4:
+    print("End")
+    i=i+1
